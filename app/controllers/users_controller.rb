da83c4d54890cc845e8de43f27975146396cc1e6
mulan04 <22234264+mulan04@users.noreply.github.com>
2023-10-31 12:05:40 +0100

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
     message = false
 
     user = User.where("id = '#{params[:user][:id]}'")[0]
+    user2 = User.where("id = '#{params[:user][:id]}'")[0]
 
     if user
       user.update(user_params_without_password)
